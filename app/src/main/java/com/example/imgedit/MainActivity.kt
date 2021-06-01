8c83078a8b6dc604c40298df83e8ea5506e3a38e
Andrey <phobos05@bk.ru>
2021-06-01 09:20:51 +0700

    2

--- app/src/main/java/com/example/imgedit/MainActivity.kt
+++ app/src/main/java/com/example/imgedit/MainActivity.kt
@@ -1,41 +1,78 @@
 package com.example.imgedit
 
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Gallery
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.rotationMatrix
+import com.bumptech.glide.Glide
+
 
 
 class MainActivity : AppCompatActivity() {
-    val GALLERY_REQUEST = 1
+
+    private val IMAGE_PICK_GALLERY_CODE = 300
+    lateinit var image: ImageView
+    lateinit var imageViewResult: ImageView
+    private var image_Uri: Uri? = null
+    var button: Button?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        image = findViewById(R.id.imageView)
+        imageViewResult=findViewById(R.id.imageViewResult)
+        Glide.with(this)
+            /*.load("https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png")
+            .error(R.drawable.err_foreground)
+            .into(image)*/
+
+        image.setOnClickListener(View.OnClickListener {
+            picFromGallery()
+        })
 
 
-        val image:ImageView = findViewById(R.id.imageView)
-        /*Picasso.with(this)
-                .load("https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png")
-                .error(R.drawable.err_foreground)
-                .into(imageView)*/
+        button=findViewById(R.id.buttonRotate)
+        button?.setOnClickListener {
+            Glide.with(applicationContext)
+                .asBitmap()
+                .load(image_Uri)
+                .transform()
+                .into(imageViewResult)
 
-       image.setOnClickListener(View.OnClickListener {
-           val photoPickerIntent = Intent(Intent.ACTION_PICK)
-           photoPickerIntent.type = "image/*"
-           startActivityForResult(photoPickerIntent, GALLERY_REQUEST)
+        }
+    }
 
-       })
+    private fun picFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE)
+    }
 
-
-
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                image_Uri = data!!.data
+                image?.setImageURI(image_Uri)
+                Log.d(TAG, image_Uri.toString())
+            } else if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                image.setImageURI(image_Uri)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 
+
 }
+
+
 
 
